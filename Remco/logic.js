1209f60d06ee8cfbb27107d7c2d68e2b240d032e
unknown <remcomooij@gmail.com>
2020-03-27 18:38:26 -0400

    changed

--- Remco/logic.js
+++ Remco/logic.js
@@ -10,37 +10,43 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: API_KEY
 }).addTo(map);
 
+
 d3.json("human_trafficking.json").then((data) => {
-  var traffickingCountries = data.citizenship;
+  var traffickingCountries = data.yearOfRegistration;
   console.log(traffickingCountries);
 });
 
 
-function colorCountry(id) {
+
+// function colorCountry(id) {
+//   for (var i = 0; i < human_trafficking.length; i++) {
+//     var countryList = human_trafficking.citizenship;
+//     console.log(countryList);
+//   };
 
 
-  traffickingCountries.forEach((country) => {
-    switch (id) {
-    case country:
-      return "yellow";
-    default:
-      return "black";
-    }
-  });
-};
+//   traffickingCountries.forEach((country) => {
+//     switch (id) {
+//     case country:
+//       return "yellow";
+//     default:
+//       return "black";
+//     }
+//   });
+// };
 
 
 var countries = "countries.geojson"
 
-d3.json(countries, function(data) {
-  L.geoJson(data, {
-    style: function(feature) {
-      return {
-        color: "white",
-        fillcolor: colorCountry(feature.id),
-        fillOpacity: 0.5,
-        weight: 1.5
-      };
-    }
-  }).addTo(map);
-});
+// d3.json(countries, function(data) {
+//   L.geoJson(data, {
+//     style: function(feature) {
+//       return {
+//         color: "white",
+//         fillcolor: colorCountry(feature.id),
+//         fillOpacity: 0.5,
+//         weight: 1.5
+//       };
+//     }
+//   }).addTo(map);
+// });
