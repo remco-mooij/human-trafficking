d1265e182a9ac38f60049e87eea7f6afc37e6caa
Adam <adam.iona@alumni.unc.edu>
2020-04-02 19:48:28 -0400

    Added app2.js

--- Adam/Resources/app.js
+++ Adam/Resources/app.js
@@ -3,14 +3,62 @@ const tableData = data;
 
 const tableBody = d3.select("tbody");
 
-// Append rows to table
+/* d3.csv("../Adam/Resources/trafficking_report.csv", function(d) {
+    return {
+        "Base Country": d["Base Country"],
+        "Base Country Latitude": d["Base Country Lat"]
+    };
+}); */
+
+/* var datavals = Object.values(tableData);
+console.log(datavals[0]); */
+
+/* d3.csv("../Adam/Resources/trafficking_report.csv").then(function(data){
+    console.log(data);
+}); */
+
+
+/* Object.forEach((key) => {
+
+    /* const arr = Object.keys(data).map((key) => [key, data[key]]);
+console.log(arr[0]); 
+
+}) */
+
+
+
+function AddData(data) {
+
+    tableBody.html("");
+
+
+    //for (var i= 0; i < data.)
+
+     // Loop through data and console.log each object
+    data.forEach((dataRow) => {
+        
+        const row = tableBody.append("tr");
+
+        Object.values(dataRow).forEach((value) => {
+            console.log(value);
+            let cell = row.append("td");
+            cell.text(value);
+
+        });
+    }); 
+
+}
+
+AddData(tableData);
+
+/* // Append rows to table
 function AddData(data) {
 
     tableBody.html("");
 
     data.forEach((dataRow) => {
 
-        const row = tableBody.append("tr");
+        var row = tableBody.append("tr");
 
         Object.values(dataRow).forEach((value) => {
             //console.log(key, value);
@@ -22,4 +70,4 @@ function AddData(data) {
     });
 }
 
-AddData(tableData);
+AddData(tableData); */
---
+++ Adam/app2.js
@@ -0,0 +1,147 @@
+const tableBody = d3.select("tbody");
+
+
+
+d3.json("trafficking.json", function(d) {
+    var data = Object.values(d);
+
+    var tableData = [];
+
+    // data = Object.values(data)
+
+    console.log(Object.values(data[0])[1])
+    
+    for (var i = 0; i < data.length; i++) {
+        tableData.push(
+            {"Base Country": Object.values(data[i])[1],
+            "Base Country Lat": Object.values(data[i])[2],
+            "Base Country Lng": Object.values(data[i])[3],
+            "Relation Country": Object.values(data[i])[4],
+            "Relation Country Lat": Object.values(data[i])[5],
+            "Relation Country Lng": Object.values(data[i])[6],
+            "Men": Object.values(data[i])[7],
+            "Women": Object.values(data[i])[8],
+            "Children": Object.values(data[i])[9],
+            "Sex Trafficking": Object.values(data[i])[10],
+            "Forced Labor": Object.values(data[i])[11],
+            "Debt Bondage": Object.values(data[i])[12],
+            "Description": Object.values(data[i])[13]
+        })
+    }
+
+
+    function init() {
+
+        console.log(tableData);
+
+        AddData(tableData);
+
+        clickButton("Base Country");
+
+    }
+
+    function AddData(data) {
+
+        tableBody.html("");
+    
+    
+         // Loop through data and console.log each object
+        data.forEach((dataRow) => {
+            
+            const row = tableBody.append("tr");
+    
+            Object.values(dataRow).forEach((value) => {
+                console.log(value);
+                let cell = row.append("td");
+                cell.text(value);
+    
+            });
+        }); 
+    
+    } 
+    
+    
+    
+    function clickButton(term){
+    
+        button.on("click", function() {
+    
+            //Prevent web page from refreshing
+            d3.event.preventDefault();
+        
+            //Accept user input, get value property of input element
+            var inputElement = d3.select("#term");
+            var inputValue = inputElement.property("value");
+        
+            if (term == "Base Country") {
+                var filteredData = tableData.filter(tableData => tableData["Base Country"] === inputValue);
+            }
+        
+            else {
+                var filteredData = tableData.filter(tableData => tableData["Relation Country"] === inputValue);
+            }
+        
+            // Empty table data before query
+            d3.select("tbody").html("");
+        
+             //Loop through filtered data
+            filteredData.forEach((dataRow) => {
+                var row = tbody.append("tr");
+                Object.entries(dataRow).forEach(([key, value]) => {
+                    cell = tbody.append("td");
+                    cell.text("value");
+                });
+        
+            });
+        });  
+        
+    };
+    
+       
+    
+    // On change to the DOM, call getData()
+    d3.selectAll("#selFilterQuery").on("change", getData);
+    
+    // Function called by DOM changes
+    function getData() {
+      
+      var dropdownMenu = d3.select("#selFilterQuery");
+      // Assign the value of the dropdown menu option to a variable
+      var term = dropdownMenu.property("value");
+    
+      clickButton(term);
+    
+    };
+    
+    // Change the search description once the dropdown is clicked
+    const areaSelect = document.querySelector(`[id="selFilterQuery"]`);
+    
+    areaSelect.addEventListener(`change`, (e) => {
+      // log(`e.target`, e.target);
+      const select = e.target;
+    
+      const value = select.value;
+    
+      const text = select.selectedOptions[0].text;
+      
+      d3.select("#SearchTerm").text("Enter a " + text);
+    
+      // Change the placeholder according to dropdown option
+      if (text == 'Base Country') {
+        document.getElementById("term").placeholder = "Afghanistan";
+      }
+    
+      else {
+        document.getElementById("term").placeholder = "United States";
+      }
+    
+      
+    });
+    
+    init();
+
+});
+
+
+
+
